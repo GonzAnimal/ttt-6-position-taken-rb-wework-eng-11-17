c9daf203728a260f781c1d283dfab52881ae6911
Gonzalo Esperon <gonzalo.esperon@wework.com>
2018-05-18 18:42:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,19 @@ board = ["",""," "," ","X"," "," "," ",""]
 
 def position_taken?(board, index)
 
-  taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-  else
-    taken = true
-  end
-  taken
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
 
+  ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
 end
+
+puts position_taken?(board, 0)
+puts position_taken?(board, 1)
+puts position_taken?(board, 2)
+puts position_taken?(board, 3)
+puts position_taken?(board, 4)
